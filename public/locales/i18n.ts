98a539c01782e265ee483288a5123dad2351c002
NoOseGG <hoc751@gmail.com>
2024-06-05 01:14:03 +0300

    added default language

--- public/locales/i18n.ts
+++ public/locales/i18n.ts
@@ -1,15 +1,13 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend'
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
     .use(initReactI18next)
-    .use(LanguageDetector)
     .use(Backend)
     .init({
         debug: true,
-        fallbackLng: 'en',
+        lng: 'en',
     })
 
 export default i18n
