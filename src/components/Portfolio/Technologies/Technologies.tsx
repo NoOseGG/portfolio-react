4f4c868234cf5317da3896a1f84eb615252c9b0c
NoOseGG <hoc751@gmail.com>
2024-06-10 01:01:31 +0300

    added multi language in technologies

--- src/components/Portfolio/Technologies/Technologies.tsx
+++ src/components/Portfolio/Technologies/Technologies.tsx
@@ -6,16 +6,21 @@ import Container from "../../Container/Container";
 import scroll from "/src/assets/scroll.svg";
 import { technologiesData } from "./data/data";
 import Item from "./Item/Item";
+import { useTranslation } from "react-i18next";
 
 const Technologies: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <section className={styles.technologies}>
       <Container>
         <div className={styles.technologies__inner}>
           <img src={scroll} className={styles.scroll} />
-          <h3 className={styles.technologies__title}>Technologies</h3>
+          <h3 className={styles.technologies__title}>
+            {t("technologies-title")}
+          </h3>
           <p className={styles.technologies__subtitle}>
-            The technologies that I studied and used when creating projects.
+            {t("technologies-subtitle")}
           </p>
         </div>
         <div className={styles.item__container}>
