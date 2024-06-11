c2ab8b7eb3717fd19c66e26dcf570b96d46e502e
NoOseGG <hoc751@gmail.com>
2024-06-11 18:54:32 +0300

    added close logics in burger menu

--- src/components/Portfolio/Header/BurgerMenu/BurgerMenu.tsx
+++ src/components/Portfolio/Header/BurgerMenu/BurgerMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import styles from "./BurgerMenu.module.css";
 
@@ -7,20 +7,40 @@ import SelectLanguage from "./SelectLanguage/SelectLanguage";
 
 const BurgerMenu: React.FC = () => {
   const [isOpen, setIsOpen] = useState<Boolean>(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const btnRef = useRef<HTMLDivElement>(null);
 
   const handleClick = () => {
     setIsOpen((prev) => !prev);
+    console.log(isOpen);
   };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      menuRef.current &&
+      !btnRef.current?.contains(event.target as Node) &&
+      !menuRef.current.contains(event.target as Node)
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
-      <div className={styles.burger__btn} onClick={handleClick}>
+      <div className={styles.burger__btn} onClick={handleClick} ref={btnRef}>
         <span className={styles.burger__line}></span>
         <span className={styles.burger__line}></span>
         <span className={styles.burger__line}></span>
       </div>
       {isOpen && (
-        <div className={styles.burger__menu}>
+        <div className={styles.burger__menu} ref={menuRef}>
           <Socials />
           <SelectLanguage />
         </div>
